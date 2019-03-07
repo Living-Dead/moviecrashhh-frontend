997c0ffe3c9414cc44f6901017e049d364e0a4b5
Living-Dead <horvth.mikls@gmail.com>
2019-03-07 21:36:14 +0100

    CREATE | new modal template

---
+++ moviecrashhh-ng6-example/src/app/modal-template/modal-template.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbActiveModal, NgbModule, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { ArticleViewComponent } from '../article-view/article-view.component';
+
+
+@Component({
+  selector: 'app-modal-template',
+  templateUrl: './modal-template.component.html',
+  styleUrls: ['./modal-template.component.scss']
+})
+export class ModalTemplateComponent implements OnInit {
+
+  constructor(private modalService: NgbModal, private router : Router) { }
+
+public close() {
+  		this.modalService.dismissAll(); 
+  		this.router.navigate(['/articles-list/list/2019-01-02']);		
+  	}
+  ngOnInit() {
+  	console.log(this.router.url);
+  	// router.url 
+  	// TODO api call
+  }
+
+}
