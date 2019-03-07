ea823e275a4e6e22f970f33a640f4936151aabd8
Living-Dead <horvth.mikls@gmail.com>
2019-03-07 21:46:04 +0100

    ADD | features and styles

--- moviecrashhh-ng6-example/src/app/blog/blog.component.ts
+++ moviecrashhh-ng6-example/src/app/blog/blog.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
+	blog: any;
+
   constructor() { }
 
   ngOnInit() {
+  	this.blog = 'dfjhjkfhskjfhsd'
   }
 
 }
