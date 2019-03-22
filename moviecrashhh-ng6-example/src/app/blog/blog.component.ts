60b0110419ded2da7a9ef69d480d55e175699a52
Living-Dead <horvth.mikls@gmail.com>
2019-03-22 20:56:27 +0100

    ADD | auth service to blog component

--- moviecrashhh-ng6-example/src/app/blog/blog.component.ts
+++ moviecrashhh-ng6-example/src/app/blog/blog.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+	selector: 'app-blog',
+	templateUrl: './blog.component.html',
+	styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
 
 	blog: any;
+	account$: any;
 
-  constructor() { }
+	constructor(private auth: AuthService) {
 
-  ngOnInit() {
-  	this.blog = 'dfjhjkfhskjfhsd'
-  }
+		this.auth.account().subscribe((data) => {
+			this.account$ = data;
+			console.log('blog', data);
+			this.auth.setLoggedIn(this.account$.account.isLoggedIn);
+		}
+		);
+	}
 
+	ngOnInit() {
+		this.blog = 'dfjhjkfhskjfhsd'
+	}
 }
