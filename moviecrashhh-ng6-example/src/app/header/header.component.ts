911952e2e7b8b71301108482528dcc51b1cf635f
Living-Dead <horvth.mikls@gmail.com>
2019-03-22 21:09:54 +0100

    ADD | logout to header

--- moviecrashhh-ng6-example/src/app/header/header.component.ts
+++ moviecrashhh-ng6-example/src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,19 +11,30 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   header: any;
+  logout$: any;
   @Input('header') headerData: any;
 
 
-  constructor() {
+  constructor(private apiService: ApiService,
+    private auth: AuthService,
+    private router: Router) { }
 
-
+  logout(data) {
+    this.apiService.logOut(data).subscribe((data) => {
+      this.logout$ = data;
+      console.log('data', data);
+      if (this.logout$.success) {
+        this.router.navigate(['']);
+      }
+    }
+    );
   }
 
   ngOnInit() {
     this.header = {
-    name: this.headerData,
-  }
-  console.log('header', this.header);
-    console.log('headerData',this.headerData.id + 20);
+      name: this.headerData,
+    }
+    console.log('header', this.header);
+    console.log('headerData', this.headerData.id + 20);
   }
 }
