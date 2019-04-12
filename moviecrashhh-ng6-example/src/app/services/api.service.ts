cb64cc6f06147bc5be26c94405aed7de89a7909b
Living-Dead <horvth.mikls@gmail.com>
2019-04-12 17:19:55 +0200

    FIX | ApiService

--- moviecrashhh-ng6-example/src/app/services/api.service.ts
+++ moviecrashhh-ng6-example/src/app/services/api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -9,57 +10,11 @@ export class ApiService {
 
 	constructor(private http: HttpClient, private router: Router) { }
 
-	getData(data) {
-		console.log(this.http.get('http://localhost:8080'));
-		if (data === 'ok') {
-			return this.http.get('http://localhost:8080/list');
-			/*return [
-				{
-				"data": {
-					"id": 834,
-					"first_name": "GS",
-					"last_name": "Shahid",
-					"phone": "03215110224",
-					"role": null,
-					"email": "test@example.com",
-					"picture": {
-					  "url": null,
-					  "thumb": {
-						"url": null
-					  }
-					},
-					"address": "Nishtar Colony",
-					"city_id": 2,
-					"provider": "email",
-					"uid": "test@example.com"
-					}
-			},
-			{
-				"data": {
-				"id": 999,
-				"first_name": "GS",
-				"last_name": "Shahid",
-				"phone": "03215110224",
-				"role": null,
-				"email": "test@example.com",
-				"picture": {
-				  "url": null,
-				  "thumb": {
-					"url": null
-				  }
-				},
-				"address": "Nishtar Colony",
-				"city_id": 2,
-				"provider": "email",
-				"uid": "test@example.com"
-			  }
-			}
-		];*/
-		}
+	getData() {
+		return this.http.get('http://localhost:8080/list');
 	}
 
 	getUserDetails(email, password) {
-		//post data missing(here you pass email and  password)
 		let data = {
 			"email": email,
 			"password": password
@@ -69,17 +24,7 @@ export class ApiService {
 	}
 
 	register(data: Object) {
-
-		console.log(data)
 		return this.http.post('http://localhost:8080/register', data);
-
-	}
-
-	logOut(logout) {
-		let data = {
-			"logout": logout,
-		}
-		return this.http.post('http://localhost:8080/logout', data, { withCredentials: true });
 	}
 
 }
